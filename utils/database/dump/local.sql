8d3d0aea086968652fbf9a1ecfabef4f27995d96
tncrazvan <tangent.jotey@gmail.com>
2017-03-23 10:01:44 +0100

    article version added

--- utils/database/dump/local.sql
+++ utils/database/dump/local.sql
@@ -1,84 +1,132 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versione server:              5.5.32 - MySQL Community Server (GPL)
--- S.O. server:                  Win32
--- HeidiSQL Versione:            9.4.0.5125
--- --------------------------------------------------------
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: local
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Current Database: `local`
+--
 
--- Dump della struttura del database local
-CREATE DATABASE IF NOT EXISTS `local` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `local` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
 USE `local`;
 
--- Dump della struttura di tabella local.test_table
-CREATE TABLE IF NOT EXISTS `test_table` (
+--
+-- Table structure for table `test_table`
+--
+
+DROP TABLE IF EXISTS `test_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test_table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT 'unknown title',
   `id_fd` varchar(254) NOT NULL,
-  `remote_id` int(10) unsigned NOT NULL DEFAULT '0',
   `shared_id` int(10) unsigned NOT NULL DEFAULT '0',
   `status` varchar(50) NOT NULL DEFAULT 'final',
+  `versione` int(11) NOT NULL DEFAULT '1',
+  `remote_id` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=151 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=182 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dump dei dati della tabella local.test_table: ~6 rows (circa)
+--
+-- Dumping data for table `test_table`
+--
+
+LOCK TABLES `test_table` WRITE;
 /*!40000 ALTER TABLE `test_table` DISABLE KEYS */;
-INSERT INTO `test_table` (`id`, `title`, `id_fd`, `remote_id`, `shared_id`, `status`) VALUES
-	(33, 'unknown title', 'unipg', 0, 0, 'final'),
-	(34, 'unknown title', 'unipg', 0, 0, 'final'),
-	(35, 'unknown title', 'unipg', 0, 0, 'final'),
-	(36, 'unknown title', 'unipg', 0, 0, 'final'),
-	(37, 'unknown title', 'unipg', 0, 0, 'final'),
-	(111, 'unknown title', 'unipg', 0, 0, 'final');
+INSERT INTO `test_table` VALUES (33,'unknown title','unipg',0,'final',1,0),(34,'unknown title','unipg',0,'final',1,0),(35,'unknown title','unipg',0,'final',1,0),(36,'unknown title','unipg',0,'final',1,0),(37,'unknown title','unipg',0,'final',1,0),(111,'unknown title','unipg',0,'final',1,0),(175,'unknown title','unimil',211,'final',1,1),(176,'unknown title','unimil',213,'final',1,2),(177,'unknown title','unimil',263,'final',1,3),(178,'unknown title','unimil',286,'final',1,4),(179,'unknown title','unimil',304,'final',1,5),(181,'test_title','unimil',382,'final',1,6);
 /*!40000 ALTER TABLE `test_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `update_log` AFTER UPDATE ON `test_table` FOR EACH ROW insert into update_log(local_id) values(NEW.id) */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
--- Dump della struttura di tabella local.tmp_update_log
-CREATE TABLE IF NOT EXISTS `tmp_update_log` (
+--
+-- Table structure for table `tmp_update_log`
+--
+
+DROP TABLE IF EXISTS `tmp_update_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tmp_update_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `local_id` int(10) unsigned NOT NULL,
   `status` varchar(50) NOT NULL DEFAULT 'final',
   PRIMARY KEY (`id`),
   KEY `FK_tmp_update_log_update_log` (`local_id`),
   CONSTRAINT `FK_tmp_update_log_update_log` FOREIGN KEY (`local_id`) REFERENCES `update_log` (`local_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dump dei dati della tabella local.tmp_update_log: ~0 rows (circa)
+--
+-- Dumping data for table `tmp_update_log`
+--
+
+LOCK TABLES `tmp_update_log` WRITE;
 /*!40000 ALTER TABLE `tmp_update_log` DISABLE KEYS */;
+INSERT INTO `tmp_update_log` VALUES (38,33,'final'),(39,34,'final'),(40,35,'final'),(41,36,'final'),(42,37,'final'),(43,111,'final'),(44,111,'final'),(45,111,'final'),(46,111,'final');
 /*!40000 ALTER TABLE `tmp_update_log` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- Dump della struttura di tabella local.update_log
-CREATE TABLE IF NOT EXISTS `update_log` (
+--
+-- Table structure for table `update_log`
+--
+
+DROP TABLE IF EXISTS `update_log`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `update_log` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `local_id` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`),
   KEY `FK_update_log_test_table` (`local_id`),
   CONSTRAINT `FK_update_log_test_table` FOREIGN KEY (`local_id`) REFERENCES `test_table` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=52 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dump dei dati della tabella local.update_log: ~0 rows (circa)
+--
+-- Dumping data for table `update_log`
+--
+
+LOCK TABLES `update_log` WRITE;
 /*!40000 ALTER TABLE `update_log` DISABLE KEYS */;
-INSERT INTO `update_log` (`id`, `local_id`) VALUES
-	(38, 33),
-	(39, 34),
-	(40, 35),
-	(41, 36),
-	(42, 37),
-	(43, 111);
+INSERT INTO `update_log` VALUES (38,33),(39,34),(40,35),(41,36),(42,37),(43,111),(44,111),(45,111),(46,111);
 /*!40000 ALTER TABLE `update_log` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
--- Dump della struttura di trigger local.update_log
-SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
-DELIMITER //
-CREATE TRIGGER `update_log` AFTER UPDATE ON `test_table` FOR EACH ROW insert into update_log(local_id) values(NEW.id)//
-DELIMITER ;
-SET SQL_MODE=@OLDTMP_SQL_MODE;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-23 10:01:14
--- utils/database/dump/shared.sql
+++ utils/database/dump/shared.sql
@@ -1,41 +1,62 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versione server:              5.5.32 - MySQL Community Server (GPL)
--- S.O. server:                  Win32
--- HeidiSQL Versione:            9.4.0.5125
--- --------------------------------------------------------
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
+--
+-- Host: localhost    Database: shared
+-- ------------------------------------------------------
+-- Server version	5.7.17-0ubuntu0.16.04.1
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Current Database: `shared`
+--
 
--- Dump della struttura del database shared
-CREATE DATABASE IF NOT EXISTS `shared` /*!40100 DEFAULT CHARACTER SET latin1 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `shared` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
 USE `shared`;
 
--- Dump della struttura di tabella shared.test_table
-CREATE TABLE IF NOT EXISTS `test_table` (
+--
+-- Table structure for table `test_table`
+--
+
+DROP TABLE IF EXISTS `test_table`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test_table` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(255) NOT NULL DEFAULT 'unknown title',
   `remote_id` int(10) NOT NULL,
   `id_fd` varchar(254) NOT NULL,
   `status` varchar(50) NOT NULL DEFAULT 'final',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=365 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=408 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Dump dei dati della tabella shared.test_table: ~5 rows (circa)
+--
+-- Dumping data for table `test_table`
+--
+
+LOCK TABLES `test_table` WRITE;
 /*!40000 ALTER TABLE `test_table` DISABLE KEYS */;
-INSERT INTO `test_table` (`id`, `title`, `remote_id`, `id_fd`, `status`) VALUES
-	(211, 'unknown title', 1, 'unimil', 'final'),
-	(213, 'unknown title', 2, 'unimil', 'final'),
-	(263, 'unknown title', 3, 'unimil', 'final'),
-	(286, 'unknown title', 4, 'unimil', 'final'),
-	(304, 'unknown title', 5, 'unimil', 'final');
+INSERT INTO `test_table` VALUES (211,'unknown title',1,'unimil','final'),(213,'unknown title',2,'unimil','final'),(263,'unknown title',3,'unimil','final'),(286,'unknown title',4,'unimil','final'),(304,'unknown title',5,'unimil','final'),(378,'test title',6,'unimil','final'),(382,'test_title',6,'unimil','final'),(402,'unknown title',33,'unipg','final'),(403,'unknown title',34,'unipg','final'),(404,'unknown title',35,'unipg','final'),(405,'unknown title',36,'unipg','final'),(406,'unknown title',37,'unipg','final'),(407,'unknown title',111,'unipg','final');
 /*!40000 ALTER TABLE `test_table` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-23 10:01:27
