14b172051787e5adb63e6252f5f10c3f1d858da9
Razvan <tangent.jotey@gmail.com>
2017-05-26 20:37:59 +0200

    setup script changed

--- utils/setup.sh
+++ utils/setup.sh
@@ -6,10 +6,9 @@ LC_ALL=en.US.UTF-8
 add-apt-repository ppa:ondrej/php-zts
 apt-get update
 apt-get install php7.0-zts php7.0-zts-dev
-y
+
 
 apt-get install git
-y
 cd $1
 git clone https://github.com/krakjoe/pthreads.git
 cd pthreads
@@ -24,18 +23,9 @@ cd /etc/php/7.0-zts/cli/conf.d
 sudo ln -s ../../conf.d/pthreads.ini
 
 apt-get install mysql-server
-y
 mysql_secure_installation
-n
-n
-y
-y
-y
-y
 apt-get install php7.0-zts-mysql
-y
 apt-get install php7.0-zts-odbc
-y
 
 cd $1
 git clone https://github.com/tncrazvan/php_socket_io.git
@@ -46,9 +36,9 @@ mysql < database/dump/localrep.sql -u root -p
 cp $1/php_socket_io/utils/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
 service mysql restart
 apt-get install apache2
-y
+
 apt-get install libapache2-mod-php7.0
-y
+
 cd /var/www/html/* -fr
 git clone https://github.com/tncrazvan/glorep.git /var/www/html
 
@@ -59,11 +49,11 @@ mkdir files/collabrep/cache
 cp default.settings.php settings.php
 chmod 777 /var/www/html/* -R
 apt-get install php7.0-gd
-y
+
 apt-get install php7.0-mysql
-y
+
 apt-get install php7.0-mbstring
-y
+
 cd $1/php_socket_io
 php init.php
 service apache2 restart
