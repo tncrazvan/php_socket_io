a85fdca9eb4c821756b82798b77e1995fe93b5b5
Razvan <tangent.jotey@gmail.com>
2017-06-04 12:43:25 +0200

    Update Logger.php

--- utils/writers/Logger.php
+++ utils/writers/Logger.php
@@ -1,7 +1,8 @@
 <?php
 class Logger{
-    public static function put($msg,$filename=LOG_FILE){
+    public static function put($msg,$current_log_dir=LOG_DIR){
+      $time=time();
       echo $msg;
-      file_put_contents($filename, $msg, FILE_APPEND);
+      file_put_contents(WORKSPACE."utils/logs/".$current_log_dir.'_log/'.substr($time,0,strlen($current_log_dir)-LOG_FILENAME_TRUNCATE).".log", $msg, FILE_APPEND);
     }
 }
